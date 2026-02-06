3ce7878a0d9a3f28246b8102976a3baffe9fa296
Thyyd <roastingblade@gmail.com>
2026-02-06 12:01:58 +0100

    Ajout du fichier de la tâche 9 : Fonction js calculant le nombre de nombre premiers, puis affichage du temps d'exécution de celle-ci

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,87 @@
+/* Version Non optimisée */
+function countPrimeNumbers()
+{
+    let nbPrimeNumber = 0;
+    let nbDiv;
+    for(let i = 2; i <= 100; i += 1)
+    {
+        nbDiv = 0;
+        for(let j = 1; j <= i; j++)
+        {
+            if (i % j === 0) {
+                nbDiv++;
+            }
+        }
+        if(nbDiv === 2)
+            nbPrimeNumber++;
+    }
+    return nbPrimeNumber;
+}
+
+const startTime = performance.now();
+countPrimeNumbers();
+const endTime = performance.now();
+const ExecuteTime = endTime - startTime;
+
+console.log("Execution time of printing countPrimeNumbers was " + ExecuteTime + " milliseconds.");
+
+
+/*
+Version Moins optimisée :
+function countPrimeNumbers()
+{
+    let nbPrimeNumber = 0;
+    let isPrime;
+    for(let i = 2; i <= 100; i += 1)
+    {
+        isPrime = true;
+        for(let j = 2; j < i; j++)
+        {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime === true)
+            nbPrimeNumber++;
+    }
+    return nbPrimeNumber;
+}
+
+const startTime = performance.now();
+countPrimeNumbers();
+const endTime = performance.now();
+const ExecuteTime = endTime - startTime;
+
+console.log("Execution time of printing countPrimeNumbers was " + ExecuteTime + " milliseconds.");*/
+
+
+/*
+Version Optimisée :
+function countPrimeNumbers()
+{
+    let nbPrimeNumber = 1; // On Commence à 1, car 2 est un nombre premier
+        let isPrime;
+        for(let i = 3; i < 100; i += 2) // Les nombres premiers sont tous impairs, sauf 2
+        {
+            isPrime = true;
+            for(let j = 3; j <=Math.sqrt(i); j += 2) // Vu que i est forcément impair, ça ne sert à rien de regarder les diviseurs pairs
+            {
+                if (i % j === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime === true)
+                nbPrimeNumber++;
+        }
+    return nbPrimeNumber;
+}
+
+const startTime = performance.now();
+countPrimeNumbers();
+const endTime = performance.now();
+const ExecuteTime = endTime - startTime;
+
+console.log("Execution time of printing countPrimeNumbers was " + ExecuteTime + " milliseconds.");
+*/

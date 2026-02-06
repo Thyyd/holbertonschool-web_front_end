44b27d8efaaee818ffe44d2dcf864b6553954cc2
Thyyd <roastingblade@gmail.com>
2026-02-06 11:14:10 +0100

    Ajout du fichier de la tâche 0 : fonction js qui lance une alerte affichant 'Welcome Prénom Nom!' (Prénom et Nom sont des paramètres à rentrer)

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,13 @@
+function welcome(firstName, lastName)
+{
+    if (typeof firstName != "string")
+        throw new TypeError("firstName doit être un string");
+    if (typeof lastName != "string")
+        throw new TypeError("lastName doit être un string");
+    let fullName = firstName + " " + lastName;
+    function displayFullName(fullName)
+    {
+        alert("Welcome "+ fullName +"!");
+    }
+    displayFullName(fullName);
+}
